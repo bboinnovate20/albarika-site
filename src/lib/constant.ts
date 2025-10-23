2fdbe380152a280855cef319b707ea11fb14ebf0
Ibrahim <bboinnovate20@gmail.com>
2025-10-23 21:31:47 +0100

    fix

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,4 +1,4 @@
 
 export const EXAM_CARDS_API_KEY = process.env.NEXT_PUBLIC_WAEC_API_KEY;
 export const EXAM_CARDS_BASE_URL: string = process.env.NEXT_PUBLIC_EXAM_CARD_BASE_URL as string;
-export const BASE_URL: string = process.env.BASE_URL as string;
+export const BASE_URL: string = process.env.NEXT_PUBLIC_BASE_URL as string;
